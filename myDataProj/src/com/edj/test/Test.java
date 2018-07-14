4731b618ae211f2209a0396c4c131721ec60a57f
avinash <avinashabbineni@gmail.com>
2018-07-14 11:38:59 -0500

    numbers checkin

--- myDataProj/src/com/edj/test/Test.java
+++ myDataProj/src/com/edj/test/Test.java
@@ -3,7 +3,7 @@ package com.edj.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("testing data 1234");
+		System.out.println("testing data 12345678");
 	}
 
 }
